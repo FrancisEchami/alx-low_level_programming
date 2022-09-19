28595978ed2d6101c70304ae2ceea8730755bb9a
FrancisEchami <francis.ee33@gmail.com>
2022-09-19 15:00:24 -0700

    Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,13 @@
 * main - prints the string in the printf function
 *
 * Description: using the main function
-* this program prints excatly with proper grammar, but the outcome is a piece of art, followed by a new line
+* this program prints excatly with proper 
+* grammar, but the outcome is a piece of 
+* art, followed by a new line
 * Return: 0
 */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
