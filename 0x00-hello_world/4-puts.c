d95e22e446c7bb1b8e9a874a0d8987fc046e5f8c
FrancisEchami <francis.ee33@gmail.com>
2022-09-19 13:50:56 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * puts- prints the string of text
+ *
+ *main- entry point
+ *
+ * Return: 0 (if the code works)
+ */
+int main(void) 
+{
+        puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
