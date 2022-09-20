cdcf3453d37da5c2107cc4479d11408849498f7a
FrancisEchami <francis.ee33@gmail.com>
2022-09-20 12:55:48 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ long long int d;
 float e;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(a));
-printf("Size of a int:	%ld byte(s)\n", sizeof(b));
+printf("Size of an int:	%ld byte(s)\n", sizeof(b));
 printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 printf("size of a float: %ld byte(s)\n", sizeof(e));
