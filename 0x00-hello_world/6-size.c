25845ffeecb13788acfedc3c67054e60b5daf7ea
FrancisEchami <francis.ee33@gmail.com>
2022-09-20 05:13:36 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 
 /**
 *
-* main - prints the string in the printf function 
+* main - prints the size of variable types 
 *
 * Description: using the printf function
-* this program prints the size of various
+* this program prints the size of variable
 * types on the computer it is compiled and run on
 * Return: 0
 */
@@ -17,10 +17,10 @@ long li;
 long long lli;
 float f;
 
-printf("Size of an int: %ld byte(s)\n", sizeof(i));
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of a long: %ld byte(s)\n", sizeof(li));
-printf("Size of a long long: %ld byte(s)\n", sizeof(lli));
-printf("size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of a long: %d byte(s)\n", sizeof(li));
+printf("Size of a long long: %d byte(s)\n", sizeof(lli));
+printf("size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
