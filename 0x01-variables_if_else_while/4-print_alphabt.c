2bf824eb5a169a965a6cdb9564fd7c6dc38d9464
FrancisEchami <francis.ee33@gmail.com>
2022-09-20 22:42:21 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ *
+ */
+int main(void)
+{
+char lox, e, q;
+
+e = 'e';
+q = 'q';
+
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != d)
+putchar(low);
+}
+printf("\n");
+return (0);
+}
